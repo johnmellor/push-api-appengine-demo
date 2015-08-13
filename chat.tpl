97be55f7a4c073ae7514555cd6a69a42a4ef7910
Jake Archibald <jaffathecake@gmail.com>
2015-08-13 13:31:50 +0100

    Wot why isn't this pinch zoomable?

--- chat.tpl
+++ chat.tpl
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html><head>
   <title>Emojoy!</title>
-  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <meta name="viewport" content="width=device-width">
   <meta name="theme-color" content="#9C27B0">
   <link rel="manifest" href="/manifest.json">
   <link rel="icon" type="image/png" href="/static/imgs/hangouts.png" sizes="42x42">
--- js/sw/index.js
+++ js/sw/index.js
@@ -8,7 +8,7 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open('chat-static-v21').then(cache => {
+    caches.open('chat-static-v22').then(cache => {
       return Promise.all([
         '/',
         '/static/css/app.css',
@@ -26,7 +26,7 @@ self.addEventListener("install", event => {
   );
 });
 
-const cachesToKeep = ['chat-static-v21', 'chat-avatars'];
+const cachesToKeep = ['chat-static-v22', 'chat-avatars'];
 
 self.addEventListener('activate', event => {
   clients.claim();
