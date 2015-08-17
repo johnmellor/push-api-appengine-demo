a1eda3306601857a984e825e9d26fd9ce68ab991
Jake Archibald <jaffathecake@gmail.com>
2015-08-17 11:28:13 +0100

    More login fun

--- js/sw/index.js
+++ js/sw/index.js
@@ -19,6 +19,11 @@ self.addEventListener("install", event => {
         let request = new Request(url, {credentials: 'include'});
         return fetch(request).then(response => {
           if (!response.ok) throw Error("NOT OK");
+          if (response.url != request.url) {
+            // needs login
+            registration.unregister();
+            throw Error("Needs login");
+          }
           return cache.put(request, response);
         });
       }));
